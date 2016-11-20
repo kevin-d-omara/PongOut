6cea87c2e391d9cec744dfe1e518cc3c02a9ae8b
Kevin O'Mara <kevin.d.omara@gmail.com>
2016-11-20 13:21:12 -0800

    +paddle prefab & spawn via BoardController

---
+++ Assets/Scripts/Controllers/PaddleController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleController : MonoBehaviour
+{
+    public float speed;
+
+    private PlayerID playerID;
+
+    public void SetupPaddle(PlayerID playerID)
+    {
+        this.playerID = playerID;
+        // change color
+    }
+
+    private void Update()
+    {
+        float moveVertical = Input.GetAxis("Vertical" + (int)playerID);
+        moveVertical *= speed * Time.deltaTime;
+        gameObject.transform.Translate(0f, moveVertical, 0f);
+    }
+
+    public void MoveByForce(Vector3 force)
+    {
+        Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+        rb.AddForce(force, ForceMode2D.Impulse);
+    }
+}
