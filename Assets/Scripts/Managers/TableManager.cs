d60f703c446ccc0c93450710148498cf2bb4cd7f
Kevin O'Mara <kevin.d.omara@gmail.com>
2016-11-19 19:37:22 -0800

    bugfix: prefab being overwritten; +SetObjectSize2D() function for background/brick/etc. resizing

--- Assets/Scripts/Managers/TableManager.cs
+++ Assets/Scripts/Managers/TableManager.cs
@@ -13,15 +13,19 @@ public class TableManager : MonoBehaviour
     public float backgroundHeight = 6.2f;
 
     // prefabs
-    public GameObject brick;
-    public GameObject powerupBrick;
-    public GameObject paddle;
-    public GameObject background;
+    public GameObject brickPrefab;
+    public GameObject powerupBrickPrefab;
+    public GameObject paddlePrefab;
+    public GameObject backgroundPrefab;
+
+    private GameObject background;
+    public GameObject GetBackground() { return background; }
 
     public void SetupScene()
     {
-        // scale background
-        
+        background = Instantiate(backgroundPrefab);
+        SetObjectSize2D(background, backgroundWidth, backgroundHeight);
+        // center
 
 
         // lay bricks
@@ -31,5 +35,20 @@ public class TableManager : MonoBehaviour
         // place paddles
             // left
             // right
+    }
+
+    // Sets object to specific size in Unity Units.
+    // Ex: SetObjectSize2D(brick, .2f, .76f) sets 'brick' to .2 units wide and
+    //     .76 units tall, regardless of prior scale.
+    private void SetObjectSize2D(GameObject obj, float xUnits, float yUnits)
+    {
+        Sprite sprite = obj.GetComponent<SpriteRenderer>().sprite;
+        Bounds bounds = sprite.bounds;
+        Vector3 size = bounds.size;
+
+        float endScaleX = xUnits / size.x;
+        float endScaleY = yUnits / size.y;
+
+        obj.transform.localScale = new Vector3(endScaleX, endScaleY, 1f);
     }
 }
