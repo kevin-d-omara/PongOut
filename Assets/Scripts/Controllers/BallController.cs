0700408b24da32d82c153ece7995c5d2e7fda8ff
Kevin O'Mara <kevin.d.omara@gmail.com>
2016-11-20 10:35:36 -0800

    inspector variable to aid testing

--- Assets/Scripts/Controllers/BallController.cs
+++ Assets/Scripts/Controllers/BallController.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class BallController : MonoBehaviour
 {
+    public float startAngle = 12f;
+
     void Awake()
     {
         // testing
         Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
-        rb.AddForce(new Vector2(500f, 10f));
+        rb.AddForce(new Vector2(500f, startAngle));
         // end testing
     } 
 }
