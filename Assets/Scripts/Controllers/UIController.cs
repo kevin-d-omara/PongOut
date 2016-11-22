292b7859e2c5b6d1b29e9666a9405bb241795968
Kevin O'Mara <kevin.d.omara@gmail.com>
2016-11-21 23:23:42 -0800

    at gameover -> play again or menu

--- Assets/Scripts/Controllers/UIController.cs
+++ Assets/Scripts/Controllers/UIController.cs
@@ -1,8 +1,21 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class UIController : MonoBehaviour
 {
     public Text[] playerScoreText = new Text[2];
+
+    public void PlayGameAgain()
+    {
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+
+
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
 }
