477b3b9154372717606930b387ec51a51c05c4a4
Kevin O'Mara <kevin.d.omara@gmail.com>
2016-11-21 22:56:38 -0800

    +ingame pause option
    Pause Keys:
    - escape
    - space
    - return
    - p

---
+++ Assets/Scripts/Controllers/PauseController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PauseController : MonoBehaviour
+{
+    public List<KeyCode> pauseKeys;
+
+    private bool isPaused = false;
+
+    private void Update()
+    {
+        if (isPaused)
+        {
+            foreach (KeyCode key in pauseKeys)
+            {
+                if (Input.GetKeyDown(key))
+                {
+                    Time.timeScale = 1f;    // resume game
+                    isPaused = false;
+                }
+            }
+        }
+        else
+        {
+            foreach (KeyCode key in pauseKeys)
+            {
+                if (Input.GetKeyDown(key))
+                {
+                    Time.timeScale = 0f;    // pause game
+                    isPaused = true;
+                }
+            }
+        }
+    }
+}
